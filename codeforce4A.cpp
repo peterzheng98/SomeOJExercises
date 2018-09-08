520a06b923f7f7c5db475afe303b3400cb1be74a
peterzheng98 <33046323+peterzheng98@users.noreply.github.com>
2018-09-08 19:37:44 +0800

    Sep 8

--- codeforce4A.cpp
+++ codeforce4A.cpp
@@ -1,5 +1,7 @@
 /*
 * codeforce 4A : ？？？编程基础？？？
+特判a=2
+* Sep 8, 2018
 */
 #include<iostream>
 using namespace std;
@@ -7,6 +9,6 @@ using namespace std;
 int main(){
 int a;
 cin >> a;
-cout << (a%2==0?"YES":"NO");
+cout << ((a%2==0 && a != 2)?"YES":"NO");
 return 0;
 }
